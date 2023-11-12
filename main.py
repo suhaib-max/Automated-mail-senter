66a5d83d29c114fa4e3f86dc66258402651cdf9c
suhaib <suhaibvbm@gmail.com>
2023-11-12 12:03:07 +0530

    txt quote assigned to content in lines as string

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import random
 import smtplib
 import datetime as td
+
+# At a time and date
+now = td.datetime.now()
+# Today weekday
+week = now.weekday()
+
+if week == 6:
+    with open("quates.txt","r") as data:
+        content = data.readlines()
+        
